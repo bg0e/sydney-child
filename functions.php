4424fd4d0515ef927dab69c71ce9661f84e4f2b4
patrick.rittenhouse <patrick.rittenhouse@beaconfire-red.com>
2017-12-12 22:20:01 -0500

    fix for woocommerce and buddypress group type plugin conflict

--- functions.php
+++ functions.php
@@ -17,3 +17,9 @@ function sydney_child_enqueue() {
 }
 
 /* ADD YOUR CUSTOM FUNCTIONS BELOW */
+
+// Update CSS within in Admin
+function admin_style() {
+    wp_enqueue_style('admin-styles', get_template_directory_uri().'/admin.css');
+}
+add_action('admin_enqueue_scripts', 'admin_style');
